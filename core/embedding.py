bec91e3ac08539b83f38dcdb3c21b3542bcbcc6b
Brendan Dwyer <brendan.dwyer@ibm.com>
2020-12-03 15:29:06 -0800

    more bandit issues

--- core/embedding.py
+++ core/embedding.py
@@ -71,7 +71,8 @@ def flair_embeddings(sentences, tokenized_contents, output_file=None):
         # Getting the tokens from our own tokenized sentence!
         tokens: List[Token] = [Token(token) for token in sent_tokens]
 
-        assert len(tokens) == len(sent_tokens)
+        if len(tokens) != len(sent_tokens):
+            raise ValueError("tokens length does not match sent_tokens length")
 
         # Create new empty sentence
         sentence = Sentence()
@@ -182,7 +183,8 @@ if __name__ == "__main__":
     else:
         contents = [args.input_sentence]
 
-    assert len(contents) == len(tokenized_contents)
+    if len(contents) != len(tokenized_contents):
+        raise ValueError("contents length does not match tokenized_contents length")
     if 'BERT' in args.contextual_embedding:
         if args.output_file:
 
